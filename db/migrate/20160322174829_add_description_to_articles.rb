2f76c1fcb5e5c815e5c94e6fd1dd171e242108ee
Johan van der Klift <info@johanvanderklift.nl>
2016-03-26 15:06:05 +0000

    Changed date_time into text in db/migrate

--- db/migrate/20160322174829_add_description_to_articles.rb
+++ db/migrate/20160322174829_add_description_to_articles.rb
@@ -1,7 +1,7 @@
 class AddDescriptionToArticles < ActiveRecord::Migration
-   def change
+  def change
     add_column :articles, :description, :text
-    add_column :articles, :created_at, :date_time
-    add_column :articles, :updated_at, :date_time
+    add_column :articles, :created_at, :text
+    add_column :articles, :updated_at, :text
   end
 end
